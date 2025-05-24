7a4d3a5d85a326faae22e64e1876cbeb56022cb0
shanmukh06 <saishanmukhhk@gmail.com>
2025-05-24 22:16:49 +0530

    Added functions of retrieving and printing memory and cpu usage

---
+++ README.md
@@ -0,0 +1,11 @@
+
+# wtop
+
+Wtop is terminal app (in progress) which shows system stats such as CPU Usage, RAM usage, Disk Usage etc.
+
+
+## Platforms
+* Windows
+* Linux
+## Modules Used
+psutil
--- wtop.py
+++ wtop.py
@@ -1,2 +1,22 @@
 import psutil
-print(psutil.disk_usage('C:\\'))
+import cpuinfo
+import wmi
+
+def cpu_info():
+      cpu_info_dict = cpuinfo.get_cpu_info()
+
+      return (f'CPU Name: {cpu_info_dict["brand_raw"]}'
+              f'\nLogical Processors: {psutil.cpu_count()}\nCores: {psutil.cpu_count(logical=False)}\n'
+              f'Architecture: {cpu_info_dict["arch"]}\n'
+              f'Base Clock: {cpu_info_dict["hz_advertised_friendly"]:.2f}')
+
+def mem_info():
+      mem = psutil.virtual_memory()
+      avail_mem = mem.available
+      total_mem = mem.total
+      used_mem = mem.used
+      return (f'Total RAM: {total_mem/(1024**3):.2f} GB\nAvailable RAM: {avail_mem/(1024**3):.2f} GB\n'
+              f'Used RAM: {used_mem/(1024**3):.2f} GB')
+
+def disk_management():
+    pass
