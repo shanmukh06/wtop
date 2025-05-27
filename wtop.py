6b5487969bf57e5e41d0b813158dc46d6c3085bc
shanmukh06 <saishanmukhhk@gmail.com>
2025-05-28 00:20:27 +0530

    Added a universal Data Unit Converter function. Added Information on Swap memory Added the functionality to retrieve data on disk-usage

--- wtop.py
+++ wtop.py
@@ -2,6 +2,17 @@ import psutil
 import cpuinfo
 import wmi
 
+
+def data_unit_converter(data) -> float:
+    if (data/(1024**3)) >= 1000:
+        return f'{data/(1024**4):.2f} TB'
+    elif (data/(1024**2)) >= 1000:
+        return f'{data/(1024**3):.2f} GB'
+    elif (data/(1024**1)) >= 1000:
+        return f'{data/(1024**2):.2f} MB'
+    else:
+        return f'{data/1024:.2f} KB'
+
 def cpu_info():
       cpu_info_dict = cpuinfo.get_cpu_info()
 
@@ -12,11 +23,25 @@ def cpu_info():
 
 def mem_info():
       mem = psutil.virtual_memory()
+      swap_mem = psutil.swap_memory()
+      total_swap = swap_mem.total
+      used_swap = swap_mem.used
+      free_swap = swap_mem.free
       avail_mem = mem.available
       total_mem = mem.total
       used_mem = mem.used
-      return (f'Total RAM: {total_mem/(1024**3):.2f} GB\nAvailable RAM: {avail_mem/(1024**3):.2f} GB\n'
-              f'Used RAM: {used_mem/(1024**3):.2f} GB')
+      return (f'Total RAM: {data_unit_converter(total_mem)}\nAvailable RAM: {data_unit_converter(avail_mem)}\n'
+              f'Used RAM: {data_unit_converter(used_mem)}\n'
+              f'Total Swap: {data_unit_converter(total_swap)}\nUsed Swap: {data_unit_converter(used_swap)}\n'
+              f'Free Swap: {data_unit_converter(free_swap)}')
 
 def disk_management():
-    pass
+    disk_info = psutil.disk_partitions()
+    for disk in disk_info:
+        global disk_data_info
+        disk_data_info = psutil.disk_usage(disk.device)
+    total_disk = disk_data_info.total
+    used_disk = disk_data_info.used
+    free_disk = disk_data_info.free
+    return (f'Total Disk Space: {data_unit_converter(total_disk)}\nUsed Disk Space: {data_unit_converter(used_disk)}'
+            f'\nFree Disk Space: {data_unit_converter(free_disk)}')
