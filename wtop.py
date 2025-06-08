5745a4f593e4f20f19d50c4e9e81bce4206e21b6
shanmukh06 <saishanmukhhk@gmail.com>
2025-06-08 22:50:20 +0530

    Added a FrontEnd UI and optimized all the metric-retrieval functions.

--- wtop.py
+++ wtop.py
@@ -1,9 +1,14 @@
+from rich.console import Group
+from rich.layout import Layout
+from rich.panel import Panel
+from rich.live import Live
+from rich.progress_bar import ProgressBar
+import time
 import psutil
 import cpuinfo
 import wmi
 
-
-def data_unit_converter(data) -> float:
+def data_unit_converter(data: float) -> str:
     if (data/(1024**3)) >= 1000:
         return f'{data/(1024**4):.2f} TB'
     elif (data/(1024**2)) >= 1000:
@@ -16,32 +21,65 @@ def data_unit_converter(data) -> float:
 def cpu_info():
       cpu_info_dict = cpuinfo.get_cpu_info()
 
-      return (f'CPU Name: {cpu_info_dict["brand_raw"]}'
-              f'\nLogical Processors: {psutil.cpu_count()}\nCores: {psutil.cpu_count(logical=False)}\n'
-              f'Architecture: {cpu_info_dict["arch"]}\n'
-              f'Base Clock: {cpu_info_dict["hz_advertised_friendly"]:.2f}')
+      return {'CPU_Name' : cpu_info_dict["brand_raw"],
+              'Logical Processors': psutil.cpu_count(), 'Cores': psutil.cpu_count(logical=False),
+              'Architecture': cpu_info_dict["arch"], 'Base Clock': cpu_info_dict["hz_advertised_friendly"]}
 
 def mem_info():
       mem = psutil.virtual_memory()
       swap_mem = psutil.swap_memory()
-      total_swap = swap_mem.total
-      used_swap = swap_mem.used
-      free_swap = swap_mem.free
-      avail_mem = mem.available
-      total_mem = mem.total
-      used_mem = mem.used
-      return (f'Total RAM: {data_unit_converter(total_mem)}\nAvailable RAM: {data_unit_converter(avail_mem)}\n'
-              f'Used RAM: {data_unit_converter(used_mem)}\n'
-              f'Total Swap: {data_unit_converter(total_swap)}\nUsed Swap: {data_unit_converter(used_swap)}\n'
-              f'Free Swap: {data_unit_converter(free_swap)}')
+      return {'total_swap': swap_mem.total, 'used_swap': swap_mem.used,
+             'free_swap': swap_mem.free, 'avail_mem': mem.available,
+             'total_mem': mem.total, 'used_mem': mem.used, 'mem_percent':mem.percent}
 
 def disk_management():
     disk_info = psutil.disk_partitions()
     for disk in disk_info:
         global disk_data_info
         disk_data_info = psutil.disk_usage(disk.device)
-    total_disk = disk_data_info.total
-    used_disk = disk_data_info.used
-    free_disk = disk_data_info.free
-    return (f'Total Disk Space: {data_unit_converter(total_disk)}\nUsed Disk Space: {data_unit_converter(used_disk)}'
-            f'\nFree Disk Space: {data_unit_converter(free_disk)}')
+    return {'total_disk' : disk_data_info.total,'used_disk' : disk_data_info.used,'free_disk' : disk_data_info.free}
+
+def ui_layout():
+    header = Layout(name="header")
+    header.split_column(
+        Layout(name="upper"),
+        Layout(name="Middle"),
+        Layout(name="Lower")
+    )
+
+    def memory():
+        data = cpu_info()
+        header["upper"].size = 6
+        header["upper"].update(Panel(
+            (f"Logical Processors: {data.get('Logical Processors')}\nCores:{data.get('Cores')}"
+             f"\nArchitecture: {data.get('Architecture')}\nBase Clock: {data.get('Base Clock')}"),
+            title=data.get('CPU_Name'),
+            title_align="left"
+        )
+        )
+
+        mem_data = mem_info()
+        total = mem_data.get('total_mem')
+        used = mem_data.get('used_mem')
+        avail = mem_data.get('avail_mem')
+
+        memory_panel_content = Group(
+            f"Total Memory: {data_unit_converter(total)}",
+            f"Available Memory: {data_unit_converter(avail)}",
+            f"Used Memory: {data_unit_converter(used)}",
+            ProgressBar(total=100, completed=(used / total) * 100)
+        )
+
+        header['Middle'].update(
+            Panel(memory_panel_content, title="Memory", title_align="left")
+        )
+        return header
+
+    with Live(header, refresh_per_second=4) as live:
+        while True:
+            memory()
+            time.sleep(0.4)
+
+
+if __name__ == '__main__':
+    ui_layout()
